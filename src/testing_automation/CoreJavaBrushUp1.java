d34d24d92223589c801fd772fdc2c5263c3dd672
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-04-24 18:20:20 +0200

    added core java brushup 1

---
+++ src/testing_automation/CoreJavaBrushUp1.java
@@ -0,0 +1,20 @@
+package testing_automation;
+
+public class CoreJavaBrushUp1 {
+
+	public static void main(String[] args) {
+		
+		int myNum = 5;
+		String myName = "Mattia Cavalieri Manasse";
+		char letterM = 'm';
+		float decimal = 0.5f;
+		boolean open = true;
+		
+		System.out.println(myName);
+		System.out.println(myNum);
+		System.out.println(letterM);
+		System.out.println(decimal);
+		System.out.println(open);
+	}
+
+}
