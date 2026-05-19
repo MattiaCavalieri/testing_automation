aa9b8d491e013f66d73dbda73a55a3998a7525ee
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-05-19 08:41:09 +0200

    implemented assertions in the flow

--- src/testing_automation/UpdatedDropdown.java
+++ src/testing_automation/UpdatedDropdown.java
@@ -1,11 +1,11 @@
 package testing_automation;
 
-import java.util.List;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+// TestNG è un framework di testing -> assertion
 
 public class UpdatedDropdown {
 
@@ -13,18 +13,24 @@ public class UpdatedDropdown {
 		// inizializzo il driver e navigo verso il link di test
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		// usiamo le assertion per verificare che la checkbox "Family and Friends" NON sia selezionata
+		Assert.assertFalse(driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).isSelected());
 
-		// verifichiamo che il checkbox sia selezionato o no
-		System.out.println(driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).isSelected());
+		// verifichiamo che il checkbox "Family and Friends" sia selezionato o no e stampiamo a schermo il controllo
+		// System.out.println(driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).isSelected());
 
 		// aggiungiamo la selezione dei checkbox
 		driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).click();
 
 		// contiamo i checkbox presenti e li stampiamo in console
 		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+		
+		// usiamo un'assertion per verificare che la checkbox "Family and Friends" sia selezionata
+		Assert.assertTrue(driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).isSelected());
 
-		// verifichiamo che il checkbox sia selezionato o no
-		System.out.println(driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).isSelected());
+		// verifichiamo che il checkbox sia selezionato o no e stampiamo a schermo il controllo
+		// System.out.println(driver.findElement(By.name("ctl00$mainContent$chk_friendsandfamily")).isSelected());
 
 		// seleziono il dorpdown del numero di passeggeri per aprire la selezione del
 		// numero di passeggeri
@@ -44,9 +50,12 @@ public class UpdatedDropdown {
 
 		// termino l'operazione con il tasto Done
 		driver.findElement(By.id("btnclosepaxoption")).click();
+		
+		// utilizziamo un assertion per verificare che siano esattamente 5 passeggeri selezionati
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
 
-		// stampo in console l'output del totale dei passeggeri selezionati
-		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		// stampo in console l'output del totale dei passeggeri selezionati e lo stampiamo in console
+		// System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
 
 	}
 
