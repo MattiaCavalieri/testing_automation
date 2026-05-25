2766d85e5ce47897f2cae891b842ee6711268a52
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-05-25 08:42:28 +0200

    inserted validation method for web elements

--- src/testing_automation/DynamicDropdown.java
+++ src/testing_automation/DynamicDropdown.java
@@ -3,6 +3,7 @@ package testing_automation;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class DynamicDropdown {
 
@@ -11,11 +12,26 @@ public class DynamicDropdown {
 		// inizializzo il driver per Chrome e navigo verso il link di test
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-		
+
 		Thread.sleep(2000);
 		
+		// stampiamo l'attributo "style" prima e dopo aver cliccato "Round Trip" 
+		System.out.println(driver.findElement(By.id("Div1")).getDomAttribute("style"));
+
 		// seleziono "Round Trip" per abilitare il calendar per la data di ritorno
 		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		
+		System.out.println(driver.findElement(By.id("Div1")).getDomAttribute("style"));
+
+		// verifichiamo che l'elemento "return date" sia abilitato
+		// stampiamo a console l'esito del metodo "isEnabled()"
+		// isEnabled() non è affidabile, usiamo le proprietà css per verificarese l'opacity è a 1
+		if (driver.findElement(By.id("Div1")).getDomAttribute("style").contains("1")) {
+			System.out.println("it is enabled!");
+			Assert.assertTrue(true);
+		} else {
+			Assert.assertTrue(false);
+		}
 
 		// //a[@value='BLR'] --> xpath dell'elemento che dobbiamo selezionare per la
 		// partenza
@@ -32,16 +48,17 @@ public class DynamicDropdown {
 		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
 
 		// selezioniamo la data corrente come data di partenza
-		// 
-		// RICORDA: stai selezionando classi, quindi devi aggiungere il "." prima del nome delle classi
+		//
+		// RICORDA: stai selezionando classi, quindi devi aggiungere il "." prima del
+		// nome delle classi
 		driver.findElement(By.cssSelector(".ui-state-default.ui-state-active")).click();
-		
-		// viene inserita automaticamente la data del ritorno impostata ad una settiana dalla data di partenza. 
+
+		// viene inserita automaticamente la data del ritorno impostata ad una settiana
+		// dalla data di partenza.
 		// completiamo l'azione con il click su "search"
 		driver.findElement(By.id("ctl00_mainContent_btn_FindFlights")).click();
 		System.out.println("Ricerca completata");
-		
-		
+
 	}
 
 }
