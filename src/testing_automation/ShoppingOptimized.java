28a0ebcc8712e8ae1ab4917bd5641f91b213ace9
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-06-05 08:43:24 +0200

    added Shopping optimized class

---
+++ src/testing_automation/ShoppingOptimized.java
@@ -0,0 +1,74 @@
+package testing_automation;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ShoppingOptimized {
+
+	public static void addItems(WebDriver driver, String[] prodottiDaAggiungere) {
+		List<WebElement> prodotti = driver.findElements(By.cssSelector("h4.product-name"));
+		int conteggio = 0;
+		for (int i = 0; i < prodotti.size(); i++) {
+			// rimuoviamo il testo " - 1 Kg" dal nome prodotto
+			String[] nomeProdotti = prodotti.get(i).getText().split("-");
+			// rimuoviamo lo spazio " " dopo "Broccoli"
+			String nomeProdotto = nomeProdotti[0].trim();
+			// verifichiamo se il nomeProdotto estratto è contenuto nell'array
+			// "prodottiRichiesti"
+			// convertiamo l'array in un arrayList
+			List<String> items = Arrays.asList(prodottiDaAggiungere);
+
+			if (items.contains(nomeProdotto)) {
+				conteggio++;
+				// clicchiamo su "Add to Cart"
+				driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+				System.out.println("ho aggiunto l'item " + nomeProdotto + " al carrello!");
+				if (conteggio == prodottiDaAggiungere.length)
+					break;
+
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		// Ottimizziamo il codice pèresente nella classe "Shopping" creando un metodo
+		// separato
+		// nel main scriviamo solo il codice necessario all'esecuzione del nostro test
+		// creiamo il driver e puntiamo al sito di test per lo shopping
+		WebDriver driver = new ChromeDriver();
+
+		// l'implicity wait viene definito a livello globale non appena viene creato il
+		// driver
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/");
+		// definiamo gli items da aggiungere
+		String[] prodottiRichiesti = { "Cucumber", "Brocolli", "Beetroot" };
+
+		Thread.sleep(3000);
+		// aggiungiamo i prodotti richiesti
+		addItems(driver, prodottiRichiesti);
+
+		// clicchiamo sull'icona del carrello e procediamo al checkhout
+		driver.findElement(By.className("cart-icon")).click();
+		driver.findElement(By.className("action-block")).click();
+		// una volta aperto il pop-up del checkout, inseriamo il promo code
+		// "rahulshettyacademy"
+		driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
+		// lanciando il test, fallisce perchè non riesce ad individuare l'elemento
+		// abbiamo bisogno dell'implictly wait, definito subito dopo la creazione del driver
+		driver.findElement(By.className("promoBtn")).click();
+		
+		// verifichiamo che il testo "Code appliend.!" sia effettivamente visualizzato
+		System.out.println(driver.findElement(By.className("promoInfo")).getText());
+	}
+
+}
