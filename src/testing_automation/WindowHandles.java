fc482bdd662621cd95463af832c5455c9e940362
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-06-17 17:03:58 +0200

    added WindowHandles class

---
+++ src/testing_automation/WindowHandles.java
@@ -0,0 +1,46 @@
+package testing_automation;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHandles {
+
+	// in questa sezione vediamo come gestire un automazione con le finestre
+	// multiple
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+
+		// clicchiamo sul link lampeggiante per aprire una nuova finestra
+		driver.findElement(By.className("blinkingText")).click();
+
+		// dobbiamo fare in modo che il driver passi il suo focus sulla seconda scheda
+		// che è stata aperta
+		// dobbiamo verificare quante schede sono state aperte, tramite il loro ID,
+		// tramite getWindowHandles(), che restituisce un set di String
+		Set<String> windows = driver.getWindowHandles();
+
+		// passiamo l'id della finestra su cui vogliamo operare al metodo switchTo()
+		// usiamo Iterator per determinare il parentID e il childID tramite il metodo
+		// next()
+		Iterator<String> iter = windows.iterator();
+		String parentID = iter.next();
+		String childID = iter.next();
+
+		driver.switchTo().window(childID);
+
+		// stampiamo in console il testo del paragrafo dove è indicato l'indirizzo email
+		// da utilizzare
+		System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
+
+		// dobbiamo estrarre "mentor@rahulshettyacademy.com"
+		String testo = driver.findElement(By.cssSelector(".im-para.red")).getText();
+	}
+
+}
