e412a7d093434c0d99ea7518728ef7e0a096b95e
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-07-01 11:49:49 +0200

    completed JavascriptExecutionDemo class

--- src/testing_automation/JavascriptExecutionDemo.java
+++ src/testing_automation/JavascriptExecutionDemo.java
@@ -1,8 +1,13 @@
 package testing_automation;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class JavascriptExecutionDemo {
 
@@ -10,13 +15,25 @@ public class JavascriptExecutionDemo {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
-		
+
 		// we have to scroll the page down using JavaScriptExecutor class
 		// we have to cast driver objet to JavaScriptExecutor js object
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0,500)");
 		Thread.sleep(3000);
 		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+
+		// let's extract all the value from the table "Web Table Fix
+		// header" and let's sum them
+		List<WebElement> values = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+		int sum = 0;
+		for (int i = 0; i < values.size(); i++) {
+			sum += Integer.parseInt(values.get(i).getText());
+		}
+
+		// compare the value from sum to the value present in the page
+		int total = Integer.parseInt(driver.findElement(By.className("totalAmount")).getText().split(":")[1].trim());
+		Assert.assertEquals(sum, total);
 	}
 
 }
