11df8278fd9cd86164a1196e42e986727a096152
mcavalie <mcavalie@LIT5CG1291F7N.corp.capgemini.com>
2026-07-17 18:02:23 +0200

    updated broken link class

--- src/testing_automation/BrokenLink.java
+++ src/testing_automation/BrokenLink.java
@@ -5,30 +5,48 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class BrokenLink {
 
-	public static void main(String[] args) throws MalformedURLException, IOException, URISyntaxException, InterruptedException {
+	public static void main(String[] args)
+			throws MalformedURLException, IOException, URISyntaxException, InterruptedException {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 
 		// broken URL
 		// step 1 - get all the URL of the page
-		String url = driver.findElement(By.cssSelector("a[href*='brokenlink']")).getAttribute("href");
-		System.out.println(url);
 
 		// there are some Java methods that verify the http statuts to determine if the
 		// link is broken
 		// if status code > 400 --> broken link
-		HttpURLConnection connection = (HttpURLConnection) new URI(url).toURL().openConnection();
-		connection.setRequestMethod("HEAD");
-		connection.connect();
-		
-		int response = connection.getResponseCode();
-		System.out.println(response);
+
+		List<WebElement> links = driver.findElements(By.cssSelector("li[class='gf-li'] a"));
+
+		for (WebElement link : links) {
+			String url = link.getAttribute("href");
+			
+			System.out.println("Checking: " + url);
+			
+			HttpsURLConnection connection = (HttpsURLConnection) new URI(url).toURL().openConnection();
+			connection.setRequestMethod("HEAD");
+			connection.connect();
+			int responseCode = connection.getResponseCode();
+			System.out.println(responseCode);
+			if (responseCode > 400) {
+				System.out.println("The link with text: " + link.getText() + " is broken");
+				Assert.assertTrue(false);
+			}
+		}
+
 	}
 
 }
