647d877501703e1ded608420fff210f0b97a95e5
Mattia Cavalieri Manasse <cavalierimanasse.2010869@strudenti.uniroma1.it>
2026-07-22 21:19:28 +0200

    created testNGTutorial package and first TestNGIntro class

---
+++ src/testNGTutorial/TestNGIntro.java
@@ -0,0 +1,13 @@
+package testNGTutorial;
+
+import org.testng.annotations.Test;
+
+public class TestNGIntro {
+
+	@Test
+	public void demo() {
+		// automation code
+		System.out.println("Hello World!");
+	}
+
+}
